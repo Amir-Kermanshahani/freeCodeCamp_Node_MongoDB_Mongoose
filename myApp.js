04e5eb0a53bb78fc62bcae90d9cf567ac26b5ec0
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-09-07 16:11:40 +0330

    find edit save modified

--- myApp.js
+++ myApp.js
@@ -58,7 +58,7 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-  Person.findPersonById(personId, function(err, person) {
+  findPersonById(personId, function(err, person) {
     if(err) return console.error(err);
     person.favoriteFoods.push(foodToAdd);
     person.save(function(err, updatedPerson) {
